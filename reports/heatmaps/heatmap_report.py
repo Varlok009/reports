eab09e2f3cdede391fc29de812422d9b2954bee0
Varlok009 <varlok009@mail.ru>
2023-02-07 17:33:29 +0400

    refactoring and add time report

--- reports/heatmaps/heatmap_report.py
+++ reports/heatmaps/heatmap_report.py
@@ -2,35 +2,66 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 from reports.report import Report
+from db.data_base import DataBase as DB
+import calendar, locale
 
 
 class HeatmapReport(Report):
-    def get_heatmap_deal_report(self, partner: str | None = None) -> str:
-        deals = self.get_data()
-        deals['year_credit'] = deals['data_credit'].dt.year
-        deals['month_credit'] = deals['data_credit'].dt.month
-        deals['day_of_weak_bid'] = deals['data_bid'].dt.dayofweek
-        plt.figure(figsize=(7, 10))
-        plt.title('Количество сделок')
-        report = sns.heatmap(deals.pivot_table(index='month_credit', values='data_credit', columns=['year_credit'],
-                                               aggfunc='count'),
+    def __init__(self, partners: list | None, years: list | None, months: list | None, days: bool, aggr: str) -> None:
+        self.partners = partners
+        self.years = years
+        self.months = months
+        self.days = days
+        self.data = self.filter_data()
+        self.aggr = aggr
+
+    def filter_data(self) -> pd.DataFrame:
+        data = DB.data
+        if self.partners:
+            data = data.query(f'partner in {list(self.partners)}')
+        if self.years:
+            data = data.query(f'year_statement in {list(self.years)}')
+        if self.months:
+            data = data.query(f'month_statement in {list(self.months)}')
+        return data
+
+    def set_heatmap_settings(self) -> None:
+        locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+        if self.days:
+            self.x_axis_labels = [day.capitalize() for day in calendar.day_abbr if day]
+        if not self.months:
+            self.y_axis_labels = [month.capitalize() for month in calendar.month_abbr if month]
+        else:
+            self.y_axis_labels = [month.capitalize() for month_number, month in enumerate(calendar.month_abbr, start=0)
+                                  if month_number in self.months]
+
+    def get_heatmap_credit_report(self) -> str:
+        credit = self.data
+        credit = credit.query(f'funded_status_at != "NaN"')
+        plt.title('Кредиты')
+        plt.figure(figsize=(12, 6))
+        self.set_heatmap_settings()
+        report = sns.heatmap(credit.pivot_table(index='month_statement', values='price',
+                                                columns='year_statement' if not self.days else 'day_of_weak_statement',
+                                                aggfunc=self.aggr),
+                             xticklabels=self.x_axis_labels if self.days else 'auto',
+                             yticklabels=self.y_axis_labels,
                              annot=True, cbar=False, fmt='.1f', cmap="BuPu", linewidths=2, linecolor='gray')
         report = self.get_str_report(report.figure)
-        print('report done')
         return report
 
-    def get_heatmap_bid_report(self) -> str:
-        bid = self.get_data()
-        bid['year_bid'] = bid['data_bid'].dt.year
-        bid['month_bid'] = bid['data_bid'].dt.month
-        bid['day_of_weak_bid'] = bid['data_bid'].dt.dayofweek
-        plt.figure(figsize=(7, 10))
-        plt.title('Количество заявок')
-        report = sns.heatmap(bid.pivot_table(index='month_bid', values='data_bid', columns=['year_bid'],
-                                             aggfunc='count'),
+    def get_heatmap_statement_report(self) -> str:
+        statement = self.data
+        plt.title('Заявки')
+        plt.figure(figsize=(12, 6))
+        self.set_heatmap_settings()
+        report = sns.heatmap(statement.pivot_table(index='month_statement', values='price',
+                                                   columns='year_statement' if not self.days else 'day_of_weak_statement',
+                                                   aggfunc=self.aggr),
+                             xticklabels=self.x_axis_labels if self.days else 'auto',
+                             yticklabels=self.y_axis_labels,
                              annot=True, cbar=False, fmt='.1f', cmap="BuPu", linewidths=2, linecolor='gray')
         report = self.get_str_report(report.figure)
-        print('report done')
         return report
 
 
--- reports/report.py
+++ reports/report.py
@@ -1,32 +1,15 @@
-import pandas as pd
-import seaborn as sns
 import base64
 from io import BytesIO
-from matplotlib import pyplot as plt
+from matplotlib import pyplot
 from os.path import dirname, abspath, join
+import pandas as pd
+import csv
 
 
 class Report:
-    partners = {'rosbank': 'Росбанк',
-                'setelem': 'ДрайвКлик'}
 
     @staticmethod
-    def convert_date_time_fields(df: pd.DataFrame) -> None:
-        df['data_bid'] = pd.to_datetime(df['data_bid'], format='%d.%m.%Y')
-        df['data_credit'] = pd.to_datetime(df['data_credit'], format='%d.%m.%Y')
-        df['day_of_weak_bid'] = df['data_bid'].dt.dayofweek
-
-    @staticmethod
-    def get_data() -> pd.DataFrame:
-        path = dirname(dirname(abspath(__file__)))
-        path = join(path, 'reports/')
-        df = pd.read_csv(f'{path}data_copy.csv', delimiter=';')
-        df.columns = ['bank', 'dealer', 'dealer_city', 'data_bid', 'data_credit']
-        Report.convert_date_time_fields(df)
-        return df
-
-    @staticmethod
-    def get_str_report(plot) -> str:
+    def get_str_report(plot: pyplot) -> str:
         buf = BytesIO()
         plot.savefig(buf, format="png")
         plot = base64.b64encode(buf.getbuffer())
@@ -34,31 +17,9 @@ class Report:
         return plot
 
     @staticmethod
-    def get_deal_report(partner: str | None = None) -> str:
-        deals = Report.get_data()
-        deals['year_credit'] = deals['data_credit'].dt.year
-        deals['month_credit'] = deals['data_credit'].dt.month
-        deals['day_of_weak_bid'] = deals['data_bid'].dt.dayofweek
-        plt.figure(figsize=(7, 10))
-        plt.title('Количество сделок')
-        report = sns.heatmap(deals.pivot_table(index='month_credit', values='data_credit', columns=['year_credit'],
-                                               aggfunc='count'),
-                             annot=True, cbar=False, fmt='.1f', cmap="BuPu", linewidths=2, linecolor='gray')
-        report = Report.get_str_report(report.figure)
-        print('report done')
-        return report
-
-    @staticmethod
-    def get_bid_report(partner: str | None = None) -> str:
-        bid = Report.get_data()
-        bid['year_bid'] = bid['data_bid'].dt.year
-        bid['month_bid'] = bid['data_bid'].dt.month
-        bid['day_of_weak_bid'] = bid['data_bid'].dt.dayofweek
-        plt.figure(figsize=(7, 10))
-        plt.title('Количество заявок')
-        report = sns.heatmap(bid.pivot_table(index='month_bid', values='data_bid', columns=['year_bid'],
-                                             aggfunc='count'),
-                             annot=True, cbar=False, fmt='.1f', cmap="BuPu", linewidths=2, linecolor='gray')
-        report = Report.get_str_report(report.figure)
-        print('report done')
-        return report
+    def get_csv_report(data: pd.DataFrame) -> str:
+        path = dirname(dirname(abspath(__file__)))
+        path = join(path, 'reports/basic/')
+        file_path = f"{path}report.csv"
+        data.to_csv(file_path, encoding='utf-8', sep=';', index=False)
+        return file_path
